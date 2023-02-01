c3da1ab805fddb13069ef8985388ea9bc29aa234
Zineb Atlassi <atlassizineb@gmail.com>
2023-02-01 11:05:25 +0100

    Completato

--- js/scripts.js
+++ js/scripts.js
@@ -5,14 +5,30 @@ createApp ({
     data (){
 
         return{
-            
+            emailList: []
         }
 
     },
 
     methods: {
 
+        generateEmail() {
+
+            for(let i=0; i<10; i++) {
+
+                axios
+                .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((resp) => {
+
+                        console.log(i, resp.data.response);
+
+                        this.emailList.push(resp.data.response)
+                    }
+                )
+            }
+        }
 
     }
 
 }).mount('#app');
+
